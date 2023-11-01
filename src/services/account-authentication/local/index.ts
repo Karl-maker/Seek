17a4e2ea35de5a731ce16a4b53011e25458837e6
Karl-Johan Bailey <karl@cenedex.com>
2023-11-01 15:53:37 -0400

    refactor: inject ITokenManager

--- src/services/account-authentication/local/index.ts
+++ src/services/account-authentication/local/index.ts
@@ -1,4 +1,5 @@
 import IAccountAuthentication, { IAuthorizePayload, ICredentials, ILoginResponse, ILogoutResponse, IRefreshPayload } from "..";
+import { ITokenManager } from "../../../helpers/token";
 import JWTService from "../../../helpers/token/jwt";
 import { IAccountRepository } from "../../../modules/account-repository";
 import HTTPError from "../../../utils/error";
@@ -10,21 +11,13 @@ interface IRefreshTokenPayload {
 
 export default class LocalAccountAuthentication implements IAccountAuthentication {
     accountRepository: IAccountRepository;
-    accessToken: {
-        expiration: string;
-        privateKey: string;
-        issuer: string;
-        publicKey: string;
-    }
-    refreshToken: {
-        expiration: string;
-        privateKey: string;
-        issuer: string;
-        publicKey: string;
-    }
+    accessTokenManager: ITokenManager<IAuthorizePayload>;
+    refreshTokenManager: ITokenManager<IRefreshTokenPayload>;
 
-    constructor(accountRepository: IAccountRepository) {
+    constructor(accountRepository: IAccountRepository, accessTokenManager: ITokenManager<IAuthorizePayload>, refreshTokenManager: ITokenManager<IRefreshTokenPayload>) {
         this.accountRepository = accountRepository;
+        this.refreshTokenManager = refreshTokenManager;
+        this.accessTokenManager = accessTokenManager;
     }
 
     async login(credentials: ICredentials): Promise<ILoginResponse> {
@@ -39,22 +32,12 @@ export default class LocalAccountAuthentication implements IAccountAuthenticatio
 
         // generate tokens
 
-        const access_token = new JWTService<IAuthorizePayload>({
-            expiration: this.accessToken.expiration || "30m",
-            privateKey: this.accessToken.privateKey || "secret",
-            publicKey: this.accessToken.publicKey || "secret",
-            issuer: this.accessToken.issuer || ""
-        }).createToken({
+        const access_token = this.accessTokenManager.createToken({
             sub: account._id,
             role: account.role
         });
 
-        const refresh_token = new JWTService<IRefreshTokenPayload>({
-            expiration: this.accessToken.expiration || "30m",
-            privateKey: this.accessToken.privateKey || "secret",
-            publicKey: this.accessToken.publicKey || "secret",
-            issuer: this.accessToken.issuer || ""
-        }).createToken({
+        const refresh_token = this.refreshTokenManager.createToken({
             sub: account._id
         });
 
@@ -65,12 +48,7 @@ export default class LocalAccountAuthentication implements IAccountAuthenticatio
     }
 
     async authorize(access_token: string): Promise<IAuthorizePayload> {
-        const payload = new JWTService<IAuthorizePayload>({
-            expiration: this.accessToken.expiration || "30m",
-            privateKey: this.accessToken.privateKey || "secret",
-            publicKey: this.accessToken.publicKey || "secret",
-            issuer: this.accessToken.issuer || ""
-        }).verifyToken(access_token);
+        const payload = this.accessTokenManager.verifyToken(access_token);
 
         if (!payload) {
             throw new HTTPError(`Access token is invalid or expired`, 401);
@@ -81,12 +59,7 @@ export default class LocalAccountAuthentication implements IAccountAuthenticatio
 
     async refresh(refresh_token: string): Promise<IRefreshPayload> {
         // Verify the refresh token
-        const payload = new JWTService<IRefreshTokenPayload>({
-            expiration: this.refreshToken.expiration || "7d", // Define refresh token expiration
-            privateKey: this.refreshToken.privateKey || "refresh-secret",
-            publicKey: this.refreshToken.publicKey || "refresh-secret",
-            issuer: this.refreshToken.issuer || ""
-        }).verifyToken(refresh_token);
+        const payload = this.refreshTokenManager.verifyToken(refresh_token);
 
         if (!payload) {
             throw new HTTPError(`Refresh token is invalid or expired`, 401);
@@ -95,12 +68,7 @@ export default class LocalAccountAuthentication implements IAccountAuthenticatio
         const account = await this.accountRepository.findById(payload.sub);
 
         // Generate a new access token using the user ID
-        const new_access_token = new JWTService<IAuthorizePayload>({
-            expiration: this.accessToken.expiration || "30m",
-            privateKey: this.accessToken.privateKey || "secret",
-            publicKey: this.accessToken.publicKey || "secret",
-            issuer: this.accessToken.issuer || ""
-        }).createToken({
+        const new_access_token = this.accessTokenManager.createToken({
             sub: payload.sub,
             role: account.role
         });
