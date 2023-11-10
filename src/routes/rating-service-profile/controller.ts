23aabbc0ae536ce2dedfcc643db6625ad3b64261
Karl-Johan Bailey <karl@cenedex.com>
2023-11-10 19:04:46 -0400

    rating-event: added all events for rating service profiles

--- src/routes/rating-service-profile/controller.ts
+++ src/routes/rating-service-profile/controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import IMessengerQueue from '../../helpers/event';
 import HTTPError from '../../utils/error';
 import { IRatingServiceProfileRepository } from '../../modules/rating-service-profile-repository';
+import { IRatinfServiceProfileRemovePayload, IRatingServiceProfileAddedPayload, RatingServiceProfileTopics } from '../../events/rating-service-profile';
 
 export default class RatingServiceProfileController {
     event: IMessengerQueue;
@@ -17,8 +18,12 @@ export default class RatingServiceProfileController {
                     account_id: req['user'].id,
                 };
                 
-                await ratingServiceProfileRepository.create(data);
-                    
+                const result = await ratingServiceProfileRepository.create(data);
+                const payload: IRatingServiceProfileAddedPayload = {
+                    rating: result.element.rating,
+                    service_profile_id: result.element.service_profile_id
+                }
+                this.event.publish(RatingServiceProfileTopics.ADDED, payload);
                 res.json({
                     message: "Rating Added"
                 })
@@ -31,10 +36,16 @@ export default class RatingServiceProfileController {
         return async(req: Request, res: Response, next: NextFunction) => {
             try{
                 const { rating_service_profile_id } = req.params;
+                const rating = await ratingServiceProfileRepository.findById(rating_service_profile_id);
                 const result = await ratingServiceProfileRepository.deleteById(rating_service_profile_id);
 
                 if(!result.success) throw new HTTPError("Issue Removing Rating", 500);
-                    
+                
+                const payload: IRatinfServiceProfileRemovePayload = {
+                    rating_service_profile: rating
+                };
+                this.event.publish(RatingServiceProfileTopics.REMOVED, payload);
+
                 res.json({
                     message: "Rating Removed"
                 })
