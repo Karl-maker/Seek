eb378237d25de2c9a441663c8f995f51fb44b004
Karl-Johan Bailey <karl@cenedex.com>
2023-11-17 12:06:34 -0400

    refactor: shuffling and removing of imports

--- service/src/index.ts
+++ service/src/index.ts
@@ -5,12 +5,6 @@ import { config } from "./config";
 import NodeEvent from "./helpers/event/node";
 import IMessengerQueue from "./helpers/event";
 import MongoDB, { IMongoDB } from "./helpers/database/mongo";
-import accountRoutes from "./routes/account";
-import serviceProfileRoutes from "./routes/service-profile";
-import serviceRoutes from "./routes/service";
-import ratingServiceProfileRoutes from "./routes/rating-service-profile";
-import availabilityServiceProfileRoutes from "./routes/service-profile-availability";
-import locationRoutes from "./routes/location";
 import IServer from "./helpers/server";
 import HttpServer from "./helpers/server/http";
 import { apiV1Router } from "./routes";
@@ -33,9 +27,8 @@ const database: IMongoDB = new MongoDB(mongo_db_uri, {
     server.app.use(express.json())
     server.app.set('view engine', 'handlebars');
     server.app.engine('.handlebars', engine({extname: 'handlebars'}));
-    
     server.app.use('/api/v1', apiV1Router(server, database, event))
-
+    
     server.start('0.0.0.0', config.port, (host: string, port: number) => {
         logger.info(`running on http://${host}:${port}/`);
     });
